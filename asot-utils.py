35cdcd0589d445e7d0ef19090bfc40a73edb57de
aaronjc15128 <aaronjchauhan2@gmail.com>
2024-05-29 21:45:36 +0100

    Added timestamps() feature and remove tracklist()

--- asot-utils.py
+++ asot-utils.py
@@ -3,23 +3,79 @@ from bs4 import BeautifulSoup
 import webbrowser
 import os
 import sys
+from mutagen.mp3 import MP3
+import datetime
 
-def tracklist(episode_number, repetitions):
+def timestamps(episode_number, repetitions):
     i = 0
     for _ in range(int(repetitions)):
+        download(episode_number, 1)
+
+        epinum = int(str(episode_number).strip()) + i
+        if len(str(epinum)) == 1: epinum = f'00{epinum}'
+        elif len(str(epinum)) == 2: epinum = f'0{epinum}'
+
+        audiofile = MP3(f"ASOT_Ep_{epinum}.mp3")
+        length = audiofile.info.length
+
         epinum = int(str(episode_number).strip()) + i
         url = f'https://miroppb.com/ASOT/{epinum}'
         response = requests.get(url)
         html = response.text
         tracklist_html = str(BeautifulSoup(html, 'html.parser').find(id='tracklist'))
 
-        with open('temp.html', 'w', encoding='utf-8') as file:
+        with open('asot-tracklist.html', 'w', encoding='utf-8') as file:
             file.write(tracklist_html)
+        
+        with open('asot-tracklist.html', 'r', encoding='utf-8') as file:
+            trackcount = 0
+            for line in file:
+                if line[1:3] == 'li':
+                    trackcount += 1
 
-        absolute_path = os.path.abspath('temp.html')
+        timepertrack = length / trackcount
+
+        stamps, running = [], 0
+        for _ in range(trackcount):
+            running += timepertrack
+            stamps.append(running)
+
+        timestamps = []
+        for stamp in stamps:
+            timestamps.append(str(datetime.timedelta(seconds=stamp)))
+
+        j = 0
+        while j < len(timestamps):
+            timestamps[j] = timestamps[j][:7]
+            j += 1
+
+        epinum = int(str(episode_number).strip()) + i
+        if len(str(epinum)) == 1: epinum = f'00{epinum}'
+        elif len(str(epinum)) == 2: epinum = f'0{epinum}'
+
+        with open('asot-tracklist.html', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        lines.insert(0, '<html style="background-color:#161616;color:white"><br>')
+        lines.insert(2, f'<h3>A State of Trance {epinum}</h3>')
+        lines.append('</html>')
+
+        j, stampnum = 0, 0
+        while j < len(lines):
+            current = lines[j]
+            if current[1:3] == 'li':
+                lines[j] = f'{current[:4]}<span style="color:pink">{timestamps[stampnum]} ~ </span>{current[4:]}'
+                stampnum += 1
+            j += 1
+
+        with open('asot-tracklist.html', 'w', encoding='utf-8') as file:
+            lines = file.writelines(lines)
+
+        absolute_path = os.path.abspath('asot-tracklist.html')
         webbrowser.open(f'file://{absolute_path}', new=1)
 
-        print(f'successful {epinum}')
+        if os.path.exists(os.path.abspath(f"ASOT_Ep_{epinum}.mp3")):
+            os.remove(os.path.abspath(f"ASOT_Ep_{epinum}.mp3"))
 
         i += 1
 
@@ -36,9 +92,8 @@ def download(episode_number, repetitions):
             with open(f'ASOT_Ep_{epinum}.mp3', 'wb') as file:
                 for chunk in response.iter_content(chunk_size=1024):
                     file.write(chunk)
-            print(f'successful {epinum}')
         else:
-            print(f'failed {epinum}')
+            print(f'!! failed to download {epinum}')
 
         i += 1
 
@@ -51,11 +106,12 @@ if __name__ =="__main__":
     parameter = sys.argv[2]
     repetitions = sys.argv[3]
 
-    #(globals()[function])(parameter, repetitions) # runs function as function(parameter, repetitions)
-
     try:
         (globals()[function])(parameter, repetitions) # runs function as function(parameter, repetitions)
     except:
         print("invalid!")
 
     input()
+
+    if os.path.exists(os.path.abspath('asot-tracklist.html')):
+        os.remove(os.path.abspath('asot-tracklist.html'))
